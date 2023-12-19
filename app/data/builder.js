610d040b754bdac24d3150f859792846b6a51120
Blake <blake@bamboodigital.io>
2023-12-19 15:09:10 -0700

    update cover + ratios

--- app/data/builder.js
+++ app/data/builder.js
@@ -1946,6 +1946,361 @@ const builderData = {
         ],
       }
     },
+    "cover": {
+      name: "Cover",
+      basePrice: 8,
+      config: {
+        sizes: [
+          {
+            size: '3” x 1”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 9,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 0,
+              glowBorder: 5,
+            }
+          },
+          {
+            size: '4” x 1”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 10,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 1,
+              glowBorder: 5,
+              hiVis: 4,
+            }
+          },
+          {
+            size: '4” x 1.5”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 10,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 3,
+              glowBorder: 5,
+            }
+          },
+          {
+            size: '5” x 1”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 13,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 2,
+              glowBorder: 5,
+              hiVis: 4,
+              tacTecCarrier: 2,
+              tacTecTrainer: 3,
+              tRexArms: 0,
+            }
+          },
+          {
+            size: '5” x 1.5”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 13,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 4,
+              glowBorder: 5,
+            }
+          },
+          {
+            size: 'T.Rex Arms AC1',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 10,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 2,
+              glowBorder: 5,
+              hiVis: 4,
+            }
+          },
+          {
+            size: 'T.Rex Arms AC1 Front',
+            text: {
+              primary: {
+                placeholder: 'Up to \n2 Lines',
+                maxLength: 16,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 13,
+              glowBorder: 10,
+            }
+          },
+          {
+            size: 'T.Rex Arms AC1 Back Panel',
+            text: {
+              primary: {
+                placeholder: 'Up to \n2 Lines',
+                maxLength: 26,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 13,
+              glowBorder: 10,
+            }
+          },
+          {
+            size: '5.11 Tac Tec Carrier Trainer',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 13,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 5,
+              glowBorder: 5,
+              hiVis: 4,
+              tacTecCarrier: 2,
+              tacTecTrainer: 3,
+              tRexArms: 0,
+            }
+          },
+          {
+            size: '5.11 Tac Tec Carrier',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 13,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 4,
+              glowBorder: 5,
+              hiVis: 4,
+              tacTecCarrier: 2,
+              tacTecTrainer: 3,
+              tRexArms: 0,
+            }
+          },
+          {
+            size: '6” x 2”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 10,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 8,
+              glowBorder: 10,
+              policeBlueLine: 5,
+              sheriffBlueLine: 5,
+            }
+          },
+          {
+            size: '8” x 2”',
+            text: {
+              primary: {
+                placeholder: 'Up to \n2 Lines',
+                maxLength: 30,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 9,
+              glowBorder: 10,
+              tRexArms: 4,
+            }
+          },
+          {
+            size: '8” x 3”',
+            text: {
+              primary: {
+                placeholder: 'Up to \n2 Lines',
+                maxLength: 26,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 14,
+              glowBorder: 10,
+            }
+          },
+          {
+            size: '8” x 4”',
+            text: {
+              primary: {
+                placeholder: 'Up to \n3 \nLines',
+                maxLength: 39,
+                lines: 3,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 24,
+              glowBorder: 10,
+            }
+          },
+          {
+            size: '9” x 3”',
+            text: {
+              primary: {
+                placeholder: 'Up to 2\n Lines of Text',
+                maxLength: 26,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 15,
+              glowBorder: 15,
+            }
+          },
+          {
+            size: '10” x 2”',
+            text: {
+              primary: {
+                placeholder: 'Your Name',
+                maxLength: 13,
+                lines: 1,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 14,
+              glowBorder: 10,
+            }
+          },
+          {
+            size: '11” x 3”',
+            text: {
+              primary: {
+                placeholder: 'Up to 2 \nLines of Text',
+                maxLength: 32,
+                lines: 2,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 17,
+              glowBorder: 15,
+            }
+          },
+          {
+            size: '12” x 4”',
+            text: {
+              primary: {
+                placeholder: 'Up to \n3 \nLines of Text',
+                maxLength: 54,
+                lines: 3,
+              }
+            },
+            flagEnabled: false,
+            upsells: {
+              size: 41,
+              glowBorder: 20,
+            }
+          }
+        ],
+      },
+      form: {
+        intro: "Our Name Tape come in various sizes. For sizes 4 x 1, 5 x1 come with the option to add a flag.",
+        btnText: "Get Started",
+        steps: [
+          {
+            name: "Text & Size",
+            status: 'current',
+            input: [
+              {
+                id: 'text',
+                label: 'Text',
+                type: 'input',
+                placeholder: '',
+              },
+              {
+                id: 'size',
+                label: 'Size',
+                type: 'select',
+                placeholder: '',
+              },
+            ],
+          },
+          {
+            name: "Font & Background Colors",
+            status: 'upcoming',
+            input: [
+              {
+                id: 'textColor',
+                label: 'Text Color',
+                type: 'advancedSelect',
+                placeholder: 'Flat Spice Brown',
+              },
+              {
+                id: 'backgroundColor',
+                label: 'Background Color',
+                type: 'advancedSelect',
+                placeholder: 'Multicam Alpine',
+              },
+            ],
+          },
+          {
+            name: "Almost There",
+            status: 'upcoming',
+            input: [
+              {
+                id: 'glowBorder',
+                label: 'Add a glow in the dark border? +$10 USD',
+                type: 'checkmark',
+                placeholder: '',
+              },
+              {
+                id: 'leadTime',
+                label: 'I Agree to the Lead Time',
+                type: 'checkmark',
+                placeholder: '<strong>Lead Time:</strong>  - From your order, to design, production, QC, and shipping, takes roughly 10 business days. Don\'t worry, we\'ll keep you updated with what is going on the whole time. Check this box to confirm that you understand that your order will take roughly 10 business days to ship.',
+              },
+            ],
+          },
+        ],
+      }
+    },
   },
   colors: {
     bgColors: [
@@ -2158,6 +2513,14 @@ const builderData = {
       ratio: "3:2"
     },
     {
+      name: "Ranger Tab",
+      ratio: "40:15"
+    },
+    {
+      name: "3.5” Hex",
+      ratio: "3:3.5"
+    },
+    {
       name: "3.5” x 2”",
       ratio: "7:4"
     },
@@ -2194,6 +2557,10 @@ const builderData = {
       ratio: "8:9"
     },
     {
+      name: "HHV Helmet",
+      ratio: "4.125:1.75"
+    },
+    {
       name: "4.6” x 6.2”",
       ratio: "23:31"
     },
@@ -2216,6 +2583,10 @@ const builderData = {
     {
       name: "T.Rex Arms AC1",
       ratio: "6:1",
+    },
+    {
+      name: "Tactical Baby Gear",
+      ratio: "6:2.5",
     },
     {
       name: "T.Rex Arms AC1 Front",
@@ -2250,12 +2621,20 @@ const builderData = {
       ratio: "8:3"
     },
     {
+      name: "Agilite Plate Carrier",
+      ratio: "8:3"
+    },
+    {
       name: "8” x 4”",
       ratio: "2:1"
     },
     {
       name: "9” x 3”",
       ratio: "3:1"
+    },
+    {
+      name: "GearDynamics Plate Carrier",
+      ratio: "9.25:2.25"
     },
     {
       name: "10” x 2”",
